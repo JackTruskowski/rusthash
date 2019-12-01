f5ec91ecaa080a32d8367e44f2b327431f0b6b0c
Jack Truskowski <jtruskow@gmail.com>
2019-12-01 17:49:47 -0600

    add some benchmarking

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,6 +1,6 @@
-#[macro_use] extern crate log;
-use std::sync::Arc;
-use std::time::Duration;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+use core::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 use rand::prelude::*;
 
@@ -8,20 +8,46 @@ mod hash_table;
 
 fn main() {
 
-    let ht = Arc::new(hash_table::HashTable::new(64));
-    let mut handles = vec![];
+    //hyperparameters
+    let HT_SIZE = 1024;
+    let ADDS_PER_THREAD = 10;
+    let NUM_THREADS = 20;
 
-    for i in 0..4 {
+    let ht = Arc::new(hash_table::HashTable::new(HT_SIZE));
+    let mut handles = vec![];
+    let mut stored_keys: Vec<u32> = Vec::new();
+    let total_duration = Arc::new(Mutex::new(Duration::new(0,0)));
+
+
+    for i in 0..NUM_THREADS {
 	println!("Thread {} starting", i);
         let ht = Arc::clone(&ht);
+	let total_duration = Arc::clone(&total_duration);
+
         let handle = thread::spawn(move || {
 
-	    for _ in 0..10 {
+	    let mut total_time = Duration::new(0,0);
+
+	    for _ in 0..ADDS_PER_THREAD {
 		let key = thread_rng().gen::<u32>();
 		let value = thread_rng().gen::<u32>();
+		stored_keys.push(key);
 
+		let start = Instant::now();
 		ht.set_item(key, value);
-		println!("\tThread {}: [{}, {}]", i, key, value);
+
+		let elapsed_time = start.elapsed();
+		total_time += elapsed_time;
+
+
+		println!("\tThread {}: [{}, {}] in {:?}", i, key, value, elapsed_time);
+	    }
+
+	    //update time using a Mutex
+	    {
+		let mut num = total_duration.lock().unwrap();
+		*num += total_time;
+
 	    }
         });
 
@@ -32,7 +58,12 @@ fn main() {
         handle.join().unwrap();
     }
 
-    println!("Map contents: ");
-    ht.print_ht_contents();
+    let float_millis = total_duration.lock().unwrap().as_nanos() as f64;
+    let total_adds = (NUM_THREADS * ADDS_PER_THREAD) as f64;
 
+
+    println!("----------------------------------------");
+    println!("Total number of insertions: {:?}", (NUM_THREADS * ADDS_PER_THREAD));
+    println!("Total wall-time of insertions: {:?}", total_duration.lock().unwrap());
+    println!("Average wall-time of insertions: {}ns", (float_millis / total_adds));
 }
